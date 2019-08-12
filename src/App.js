72ffec7efac483c43396e56eb4722a480e86b64b
Richard van der Dys <richard.vanderdys@gmail.com>
2019-08-12 18:10:42 +0300

    Using function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [response, setResponse] = useState();
+
+  function handleGreeting() {
+    fetch(`/.netlify/functions/hello?name=${name}`)
+      .then(res => res.text())
+      .then(setResponse);
+  }
+
   return (
     <div className="App">
       <h1>Demo</h1>
+      <br />
+      <br />
+      <p>
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder="Your name here"
+        />
+        &nbsp;<button onClick={handleGreeting}>Say hi</button>
+      </p>
+      {response && <p className="server">Server Response: {response}</p>}
     </div>
   );
 }
